cbee65386a4b5afd037c4e5a58b7b10461b25115
Tobias Weber <t.weber@campus.lmu.de>
2023-01-10 08:20:41 +0100

    Moved comment to header

--- berd/text_classification/main.py
+++ berd/text_classification/main.py
@@ -1,4 +1,10 @@
-"""Script for training a classifier on ToxicComment dataset  with Pytorch Lightning."""
+"""
+Script for training a classifier on ToxicComment dataset  with Pytorch Lightning.
+
+This code is adapted based on
+https://curiousily.com/posts/multi-label-text-classification-with-bert-and-pytorch-lightning/
+"""  # noqa
+
 
 import argparse
 import os
@@ -17,8 +23,6 @@ from berd.text_classification.data import ToxicCommentDataModule
 from berd.text_classification.model import ToxicCommentTagger
 
 RANDOM_SEED = 42
-"""This code is adapted based on
-https://curiousily.com/posts/multi-label-text-classification-with-bert-and-pytorch-lightning/"""
 
 
 def parse_option():
